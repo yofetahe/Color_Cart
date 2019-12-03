d8ba0d9f1ce7b1782ba53bba50402ecc0911273e
Yofetahe Habtu <yofetahe@gmail.com>
2019-12-03 13:32:06 -0800

    Updates

---
+++ precache-manifest.01a3804119104fa1726059ce80873f71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2661381b871627650dfae762a74dbc9a",
+    "url": "/yofetahe/Color_Cart/index.html"
+  },
+  {
+    "revision": "b743b25acde444f02ead",
+    "url": "/yofetahe/Color_Cart/static/css/main.3d0ab1af.chunk.css"
+  },
+  {
+    "revision": "a48fbce15718fa50a58f",
+    "url": "/yofetahe/Color_Cart/static/js/2.d195d1a3.chunk.js"
+  },
+  {
+    "revision": "b743b25acde444f02ead",
+    "url": "/yofetahe/Color_Cart/static/js/main.3f9345f9.chunk.js"
+  },
+  {
+    "revision": "10083ddac9000da65689",
+    "url": "/yofetahe/Color_Cart/static/js/runtime-main.bebf5063.js"
+  },
+  {
+    "revision": "39a606266fd81d8ed5dd7cc3045ae5f4",
+    "url": "/yofetahe/Color_Cart/static/media/CartIcon.39a60626.svg"
+  },
+  {
+    "revision": "56b04b4cd009dad35fefe606dc0e8d64",
+    "url": "/yofetahe/Color_Cart/static/media/NewEngen-Logo.56b04b4c.svg"
+  }
+]);
